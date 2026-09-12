7dc87344c7c829f06841f7f0c4d640085c463d54
kuchris <kuchrisuk@gmail.com>
2026-09-12 15:16:07 +0900

    Link voice studio project to its GitHub repository

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let projectsData = [];
 let timelineData = [];
 let skillsData = null;
 let translations = {};
-const DATA_VERSION = "20260912";
+const DATA_VERSION = "20260912-2";
 
 const timelineContainer = document.getElementById("timeline-container");
 const projectsContainer = document.getElementById("projects-container");
